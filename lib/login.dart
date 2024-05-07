c4f7ec2ede65c6c8bd9be646da00562fdf9a560c
hysterix5 <denopolred@gmail.com>
2024-05-07 07:28:03 -0700

    adjusted login

--- lib/login.dart
+++ lib/login.dart
@@ -22,136 +22,138 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              Image.asset('assets/icons/acd_logo.png',
-                  height: 65.0, width: 65.0),
-              const SizedBox(height: 5),
-              const Text("Assumption College of Davao",
-                  style: TextStyle(fontSize: 16)),
-              const Text("J.P Cabaguio Avenue, Davao City",
-                  style: TextStyle(fontSize: 13)),
-              const SizedBox(height: 75.0),
-              Image.asset(
-                'assets/icons/votivate2.png',
-                height: 150.0,
-              ),
-              const SizedBox(height: 25.0),
-              const Text('Welcome Assumptionista!',
-                  style: TextStyle(
-                    fontSize: 23,
-                  )),
-              const Text(
-                'Use your ACD Google Suite account to login to Votivate',
-                style: TextStyle(
-                  fontSize: 13,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                Image.asset('assets/icons/acd_logo.png',
+                    height: 65.0, width: 65.0),
+                const SizedBox(height: 5),
+                const Text("Assumption College of Davao",
+                    style: TextStyle(fontSize: 16)),
+                const Text("J.P Cabaguio Avenue, Davao City",
+                    style: TextStyle(fontSize: 13)),
+                const SizedBox(height: 75.0),
+                Image.asset(
+                  'assets/icons/votivate2.png',
+                  height: 150.0,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              GestureDetector(
-                onTap: () async {
-                  final authResult =
-                      await AuthenticateProvider().signInWithGoogle();
-                  User? user = authResult.user;
+                const SizedBox(height: 25.0),
+                const Text('Welcome Assumptionista!',
+                    style: TextStyle(
+                      fontSize: 23,
+                    )),
+                const Text(
+                  'Use your ACD Google Suite account to login to Votivate',
+                  style: TextStyle(
+                    fontSize: 13,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+                GestureDetector(
+                  onTap: () async {
+                    final authResult =
+                        await AuthenticateProvider().signInWithGoogle();
+                    User? user = authResult.user;
 
-                  if (user == null) {
-                    // Handle unsuccessful sign-in
-                    error(context, message: "Please try again");
-                  } else {
-                    if (user.email?.endsWith("@acdeducation.com") ?? false) {
-                      final userSnapshot = await FirebaseFirestore.instance
-                          .collection('users')
-                          .doc(user.uid)
-                          .get();
+                    if (user == null) {
+                      // Handle unsuccessful sign-in
+                      error(context, message: "Please try again");
+                    } else {
+                      if (user.email?.endsWith("@acdeducation.com") ?? false) {
+                        final userSnapshot = await FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(user.uid)
+                            .get();
 
-                      final program = userSnapshot['program'];
+                        final program = userSnapshot['program'];
 
-                      if (userSnapshot.exists) {
-                        Map<String, dynamic> userData =
-                            userSnapshot.data() as Map<String, dynamic>;
-                        int? accessLevel = userData['access_level'] as int?;
-                        if (accessLevel == 1) {
-                          print(program);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: ((context) => const HomePage()),
-                            ),
-                          );
-                        } else if (accessLevel == 2) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: ((context) => const AdminHomePage()),
-                            ),
-                          );
-                        } else if (accessLevel == 3) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: ((context) =>
-                                  const SuperAdminDashboard()),
-                            ),
-                          );
+                        if (userSnapshot.exists) {
+                          Map<String, dynamic> userData =
+                              userSnapshot.data() as Map<String, dynamic>;
+                          int? accessLevel = userData['access_level'] as int?;
+                          if (accessLevel == 1) {
+                            print(program);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: ((context) => const HomePage()),
+                              ),
+                            );
+                          } else if (accessLevel == 2) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: ((context) => const AdminHomePage()),
+                              ),
+                            );
+                          } else if (accessLevel == 3) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: ((context) =>
+                                    const SuperAdminDashboard()),
+                              ),
+                            );
+                          }
+                        } else {
+                          // User doesn't exist in the Firestore database
+                          nextPageOnly(context, AgreeScreen());
                         }
                       } else {
-                        // User doesn't exist in the Firestore database
-                        nextPageOnly(context, AgreeScreen());
+                        await showGoogleLoginDialog(context);
                       }
-                    } else {
-                      await showGoogleLoginDialog(context);
                     }
-                  }
-                },
-                child: Container(
-                  height: 50.0,
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: AppColors.bgColor,
-                    borderRadius: BorderRadius.circular(30.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 2,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/icons/google_icon.png',
-                          width: 30.0, height: 30.0),
-                      const SizedBox(width: 10.0),
-                      const Text(
-                        'Login with Google',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                  },
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: AppColors.bgColor,
+                      borderRadius: BorderRadius.circular(30.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 2,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/icons/google_icon.png',
+                            width: 30.0, height: 30.0),
+                        const SizedBox(width: 10.0),
+                        const Text(
+                          'Login with Google',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 125.0,
-              ),
-              const Text(
-                'Developed by',
-                style: TextStyle(
-                  fontSize: 10.0,
+                const SizedBox(
+                  height: 125.0,
                 ),
-              ),
-              Image.asset('assets/icons/katribo.png', height: 35.0),
-            ],
+                const Text(
+                  'Developed by',
+                  style: TextStyle(
+                    fontSize: 10.0,
+                  ),
+                ),
+                Image.asset('assets/icons/katribo.png', height: 35.0),
+              ],
+            ),
           ),
         ),
       ),
